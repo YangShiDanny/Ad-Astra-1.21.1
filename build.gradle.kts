1a6f72de6057528eac061410bb557b56e0d14445
YangShiDanny <1020110927@qq.com>
2026-08-21 02:09:44 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,9 @@ repositories {
         mavenNeoforgedMeta()
         mavenFabric()
     }
-    maven{url = "https://libraries.minecraft.net/"}
+    maven {
+        url = uri("https://libraries.minecraft.net/")
+    }
     maven(url = "https://maven.teamresourceful.com/repository/maven-public/")
     maven(url = "https://maven.firstdarkdev.xyz/snapshots")
     maven(url = "https://maven.shedaniel.me")
